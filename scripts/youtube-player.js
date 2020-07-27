e9f908e4d132fd008d4baa8c58fe6f4d4bd992da
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2020-07-28 00:04:29 +0300

    Added logic to play videos when you click on a custom play button

---
+++ scripts/youtube-player.js
@@ -0,0 +1,41 @@
+const videoIDs = [
+  'IyjRgJ7L5vc'
+];
+
+// Load IFrame Player API code asynchronously.
+const iframeApi = document.createElement('script');
+iframeApi.id = 'iframe-api';
+iframeApi.src = 'https://www.youtube.com/iframe_api';
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(iframeApi, firstScriptTag);
+
+// Create an <iframe> after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  videoIDs.map(videoId => {
+    const player = new YT.Player(`${videoId}`, { // replaces the element with this ID
+      playerVars: {
+        'iv_load_policy': 3
+      },
+      videoId: videoId,
+      events: {
+        'onReady': onPlayerReady
+      }
+    });
+
+    const { f: elementInfo } = player;
+    const { style } = elementInfo;
+    style.display = 'none';
+  });
+}
+
+function onPlayerReady({ target }) {
+  const { f: elementInfo } = target;
+  const { style } = elementInfo;
+  const videoWrapperId = `${elementInfo.id}-wrapper`;
+
+  document.getElementById(videoWrapperId).onclick = function () {
+    style.display = 'block';
+    style['z-index'] = 99;
+    target.playVideo();
+  };
+}
